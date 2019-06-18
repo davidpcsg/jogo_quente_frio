469f2052a71af346e6fbe9b3e7b991082739dacb
David Duarte <davidpcsg@gmail.com>
2019-06-18 12:57:47 -0300

    nova wisard classificando

--- src/image.py
+++ src/image.py
@@ -20,7 +20,7 @@ import cv2
 
 
 #wisard
-from wisard import *
+import wisardpkg as wp
 from string import *
 from pyppm import *
 import sys
@@ -35,107 +35,88 @@ import time
 #*********************************************************************
 # the application class
 class wiz:
+    
     # init: receives the root widget
     def __init__(self):
 
         # general widget creation & variables
         self.n_classes = 3
-	self.raiz = '/home/davidpcsg/Imagens'
-        self.n_bits = 10
-	self.n_samples = 10
+        self.raiz = '/home/davidpcsg/Imagens'
+        self.n_samples = 10
         self.types = [("PPM", ".ppm")]
-        self.net = self.x = self.y = None
-	#
-	self.imgTitClass1 = cv2.imread(self.raiz + '/imgClass1.jpg',flags=cv2.IMREAD_COLOR)
-	self.imgTitClass2 = cv2.imread(self.raiz +'/imgClass2.jpg',flags=cv2.IMREAD_COLOR)
-	self.imgTitClass3 = cv2.imread(self.raiz +'/imgClass3.jpg',flags=cv2.IMREAD_COLOR)
-	self.imgTitClass4 = cv2.imread(self.raiz +'/imgClass4.jpg',flags=cv2.IMREAD_COLOR)
+        self.wsd = self.x = self.y = None
+        
+        #
+        self.imgTitClass1 = cv2.imread(self.raiz + '/imgClass1.jpg',flags=cv2.IMREAD_COLOR)
+        self.imgTitClass2 = cv2.imread(self.raiz +'/imgClass2.jpg',flags=cv2.IMREAD_COLOR)
+        self.imgTitClass3 = cv2.imread(self.raiz +'/imgClass3.jpg',flags=cv2.IMREAD_COLOR)
+        self.imgTitClass4 = cv2.imread(self.raiz +'/imgClass4.jpg',flags=cv2.IMREAD_COLOR)
         self.imgBlack = cv2.imread(self.raiz +'/black.jpg',flags=cv2.IMREAD_COLOR)
-		
-    def switch_class_name(self,class_identifier):
-        switcher = {
-            1: "Estreito",
-            2: "Normal",
-            3: "Largo",
-            4: "Parede",
-        }
-        return switcher.get(class_identifier)
-	
+    
     def switch_class_image(self,class_identifier):
         switcher = {
-            1: self.imgTitClass1,
-            2: self.imgTitClass2,
-            3: self.imgTitClass3,
-            4: self.imgTitClass4,
+            "triangulo": self.imgTitClass1,
+            "quadrado": self.imgTitClass2,
+            "circulo": self.imgTitClass3,
+            "indefinido": self.imgTitClass4,
         }
         return switcher.get(class_identifier)
 
     def train_sample(self):
 
+        classes = list()
+        examples = list()
+        addressSize = 10
+        ignoreZero  = False
+        verbose = True
+        self.wsd = wp.Wisard(addressSize, ignoreZero=ignoreZero, verbose=verbose)
+        
         for i in range(1,(self.n_classes+1)):
             self.train_class = i
             for j in range(1, self.n_samples+1):
                 file = self.raiz +'/classe'+str(i)+'_treino/picture' + str(j)+'_.ppm'
-                image = load_image(file)
-		
-		x = len(image)
-                y = len(image[0])
-
-                #  check if the image has the same size of the last one
-                if not self.x:
-                    self.x = x
-                    self.y = y
-                elif self.x != x or self.y != y:
-                    print("Image size inconsistent with the WISARD!\n")
-                    return None
-
-                # checks if the wisard was already instantiated
-                if not self.net:
-                    self.net = wisard(self.n_classes, self.n_bits, x, y)
-                    print("Created WISARD net, input size=%dx%d.\n" % (x, y))
-                    print("Each discriminator has %d RAM neurons.\n" % (self.net.n_ram))
-
-                self.net.train(self.train_class - 1, image, 0, 0)
-
+                image_src = cv2.imread(file, 0)
+              
+                image_dst = [(1 if e==255 else 0) for e in image_src.flatten()]
+                            
                 # print feedback to user
                 print("Class %d <- \"%s\" ." % (self.train_class, file))
+                
+                train_class_label = ""
+                if self.train_class == 1:
+                    train_class_label = "triangulo"
+                elif self.train_class == 2:
+                    train_class_label = "quadrado"
+                elif self.train_class == 3:
+                    train_class_label = "circulo"
+                else:
+                    train_class_label = "indefinido"
+                    
+                classes.append(train_class_label)
+                examples.append(image_dst)
+                
+        self.wsd.train(examples,classes)
 
 
     def rec_sample(self, file):
-        image = load_image(file)
-        x = len(image)
-        y = len(image[0])
-
-        #  check if the image has the same size of the last one
-        if not self.x:
-            self.x = x
-            self.y = y
-        elif self.x != x or self.y != y:
-            print("Image size inconsistent with the WISARD!\n")
-            return None
-
-        # recognise the image and print a report
-        output = self.net.stats(image, 0, 0)
-        print("\nWISARD answer for \"%s\":" % file)
-        print("Class   Sum  Recog.")
-        for i in xrange(self.n_classes):
-            print("%5d %5d %6.1f%%" % (i + 1, output[i][0], output[i][1]))
+        image = cv2.imread(file, 0)
+        image_bin = [(1 if e==255 else 0) for e in image.flatten()]
         
-	storeClassNumber = 1
+        # recognise the image and print a report
+        out = self.wsd.classify([image_bin])
+        
+        print("wisard answer:")
+        print(out[0])
+        
+        storeClassNumber = 1
         storePercent= 0
-        for i in xrange(self.n_classes):
-            if storePercent < output[i][1]:
-                storePercent = output[i][1]
-                storeClassNumber = i+1
- 	cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+        cv2.namedWindow('image',cv2.WINDOW_NORMAL)
         cv2.resizeWindow('image', 320,240)   
-	class_name = self.switch_class_name (storeClassNumber)	
-	print(class_name)
-        print("%5d" % storePercent)		
-        cv2.imshow('image',self.switch_class_image(storeClassNumber))
-	cv2.waitKey(3)
-
-
+        class_name = out[0]  
+        print(class_name)     
+        cv2.imshow('image', self.switch_class_image(class_name))
+        cv2.waitKey(3)
+        
 # wiz class end
 #*********************************************************************
 #wizzard aplication class end
@@ -164,9 +145,9 @@ def msg_img_to_rec_callback(data):
     file = image_source
     wiz.rec_sample(a,file)
 #    print(image_source)
-		
+        
 def main():
-	
+    
     rospy.init_node('pic_to_recognize')
     # Define your image topic
 #    image_topic = "/cameras/left_hand_camera/image"
@@ -179,5 +160,5 @@ def main():
 
 if __name__ == '__main__':
     a = wiz()
-    wiz.train_sample(a)	
+    wiz.train_sample(a) 
     main()
